dda44c7d65623ab23be87a67e71c64d675e4d2fe
Andrew McDonald <mcdonalda1993@gmail.com>
2014-02-27 23:56:54 +0000

    Created method signatures for implementing thens

--- Sprint3/src/test/steps/UserStorySteps.java
+++ Sprint3/src/test/steps/UserStorySteps.java
@@ -3,6 +3,8 @@ package test.steps;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import java.sql.ResultSet;
+
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
@@ -19,9 +21,18 @@ public class UserStorySteps {
 
 	private Users user;
 	private dbDriver db = new dbDriver();
+	
 	private String course;
 	private int session;
+	private int time;
+	private String studentID;
+	private int capacity;
+	private String startTime;
+	private int duration;
+	private int day;
+	private String room;
 
+	private ResultSet returnValue;
 	
 	@Given("a user, $user")
 	public void aUser(String user){
@@ -48,6 +59,41 @@ public class UserStorySteps {
 	@Given("a session, $session")
 	public void aSession(int session){
 		this.session = session;
+	}	
+	
+	@Given("a time, $time")
+	public void aTime(int time){
+		this.time = time;
+	}
+	
+	@Given("a student, $student")
+	public void aStudent(String student){
+		this.studentID = student;
+	}
+	
+	@Given("a capacity, $capacity")
+	public void aCapacity(int capacity){
+		this.capacity = capacity;
+	}
+	
+	@Given("a start time, $startTime")
+	public void aStartTime(String startTime){
+		this.startTime = startTime;
+	}
+	
+	@Given("a duration, $duration")
+	public void aDuration(int duration){
+		this.duration = duration;
+	}
+	
+	@Given("a day, $day")
+	public void aDay(int day){
+		this.day = day;
+	}
+
+	@Given("a room, $room")
+	public void aRoom(String room){
+		this.room = room;
 	}
 	
 	@When ("importing the sessions")
@@ -72,44 +118,67 @@ public class UserStorySteps {
 	@When ("I want to see details")
 	public void getCourseSessionDetails(){
 		if (user instanceof Lecturer) {//make sure it is an instance of the child class before casting
-			((Lecturer) user).getCourseSessionDetails(course);
+			returnValue = ((Lecturer) user).getCourseSessionDetails(course);
 		}
 	}
 
 	@When ("I want to book a slot for each session for the course")
 	public void bookSession(){
-		
+		if (user instanceof Admin) {//make sure it is an instance of the child class before casting
+			((Admin) user).bookSession(session, time);
+		}
 	}
 
 	@When ("I want to check if I have signed up for all compulsory sessions so that I don't fail the course.")
 	public void checkCompulsorySessions(){
-		
+		if (user instanceof Student) {//make sure it is an instance of the child class before casting
+			((Student) user).checkCompulsorySessions(studentID);
+		}
 	}
 
 	@When ("I want to create a timetable slot for a session so that rooms can be assigned to slots.")
 	public void addTimeslot(){
+		if (user instanceof Admin) {//make sure it is an instance of the child class before casting
+			((Admin) user).addTimeslot(capacity, startTime, duration, day, room);
+		}
+	}
+	
+	
+	@Then("the sessions will import correctly")
+	public void sessionImport(){
 		
 	}
 
-	
-	@When("the $modeName mode is selected")
-	public void modeIsSelected(String modeName){
+	@Then("the session is made")
+	public void checkSessionExistence(){
 		
-		currentMode = 
-			Mode.valueOf(modeName.toUpperCase());
-				
-		microwaveOven.setCurrentMode(currentMode);
 	}
-	
-	@When("a $size chicken is selected")
-	public void cookingItemName(String size)
-		throws CookeryException{
 
-		currentItem = FoodItemFactory.createChicken(size);
-
-		microwaveOven.setCurrentFoodItem(currentItem);
-
+	@Then("the session is recurring")
+	public void checkSessionRecurring(){
+		
 	}
+
+	@Then("I see the details of every session")
+	public void checkDetailsExistence(){
+		
+	}
+
+	@Then("the slots are booked")
+	public void checkSlotBooked(){
+		
+	}
+
+	@Then("the system checks")
+	public void checkCompulsory(){
+		
+	}
+
+	@Then("the system assigns the sessions to the timeslot")
+	public void checkAssignment(){
+		
+	}
+
 	
 	@Then("cooking time is $expected minutes")
 	public void cookingTimeIs(Integer expected)
@@ -119,14 +188,6 @@ public class UserStorySteps {
 			microwaveOven.getCalculatedTime();
 						
 		assertThat(actual, equalTo(expected));
-	}
-	
-	@When("a chicken weighing $weight kg is roasted")
-	public void aFoodItemWeighingKilosIsRoasted(Double weightInKilograms){
-		
-		currentItem = FoodItemFactory.createChicken(weightInKilograms);
-		
-		oven.roast(currentItem);
 	}
 	
 	@Then("oven temperature is $expected degrees celsius")
